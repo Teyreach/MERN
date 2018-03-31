e306876480dcd3e840d7f91a17a17cd16c01ebad
Robert Koszela <dorso123@gmail.com>
2018-03-31 19:27:50 +0200

    zadanie22_6

--- client/routes.js
+++ client/routes.js
@@ -18,7 +18,10 @@ if (process.env.NODE_ENV !== 'production') {
   // Require async routes only in development for react-hot-reloader to work.
   require('./modules/Post/pages/PostListPage/PostListPage');
   require('./modules/Post/pages/PostDetailPage/PostDetailPage');
+  require('./modules/Home/Home');
+  require('./modules/About/About');
 }
+
 
 // react-router setup with code-splitting
 // More info: http://blog.mxstbr.com/2016/01/react-apps-with-pages/
@@ -31,6 +34,22 @@ export default (
         });
       }}
     />
+	<Route
+      path="/home"
+      getComponent={(nextState, cb) => {
+        require.ensure([], require => {
+          cb(null, require('./modules/Home/Home').default);
+        });
+      }}
+    />
+	<Route
+      path="/about"
+      getComponent={(nextState, cb) => {
+        require.ensure([], require => {
+          cb(null, require('./modules/About/About').default);
+        });
+      }}
+    />
     <Route
       path="/posts/:slug-:cuid"
       getComponent={(nextState, cb) => {
@@ -40,4 +59,5 @@ export default (
       }}
     />
   </Route>
+  
 );
